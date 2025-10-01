143c502dd4ce4bc0095d98085c96175f4c1f7307
S-Mauryaa <github.saurabhmaurya@gmail.com>
2025-10-01 21:22:41 +0530

    Initial commit: Paytm complete solution with frontend, backend, DB setup

---
+++ frontend/src/pages/Signin.jsx
@@ -0,0 +1,22 @@
+import { BottomWarning } from "../components/BottomWarning"
+import { Button } from "../components/Button"
+import { Heading } from "../components/Heading"
+import { InputBox } from "../components/InputBox"
+import { SubHeading } from "../components/SubHeading"
+
+export const Signin = () => {
+    return <div className="flex justify-center h-screen bg-slate-300">
+    <div className="flex flex-col justify-center">
+      <div className="p-2 px-4 text-center bg-white rounded-lg w-80 h-max">
+        <Heading label={"Sign in"} />
+        <SubHeading label={"Enter your credentials to access your account"} />
+        <InputBox placeholder="saurabhmaurya@gmail.com" label={"Email"} />
+        <InputBox placeholder="123456" label={"Password"} />
+        <div className="pt-4">
+          <Button label={"Sign in"} />
+        </div>
+        <BottomWarning label={"Don't have an account?"} buttonText={"Sign up"} to={"/signup"} />
+      </div>
+    </div>
+  </div>
+}
